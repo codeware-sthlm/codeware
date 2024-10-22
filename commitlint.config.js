bb7853eccaef27ac329f9fcbf80858c87d0d27bb
Håkan Ströberg <hakan.stroberg@codeware.se>
2024-10-22 23:22:02 +0200

    chore(repo): add code quality tools

---
+++ commitlint.config.js
@@ -0,0 +1,48 @@
+const { execSync } = require('child_process');
+const { defaultConfig, RuleConfigSeverity } = require('cz-git');
+
+// Resolve scopes from Nx projects, except e2e
+const projects = JSON.parse(
+  execSync('nx show projects --exclude="*-e2e" | jq -R | jq -cs', {
+    encoding: 'utf-8'
+  })
+);
+// Allow release commits via CLI
+// and generic repository changes
+projects.push('release', 'repo');
+
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'scope-enum': [RuleConfigSeverity.Error, 'always', projects],
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'test',
+        'release'
+      ]
+    ]
+  },
+  prompt: {
+    allowBreakingChanges: ['feat', 'fix'],
+    allowCustomScopes: false,
+    allowEmptyScopes: false,
+    allowCustomIssuePrefix: false,
+    allowEmptyIssuePrefix: false
+  },
+  messages: {
+    ...defaultConfig.messages,
+    footer: defaultConfig.messages?.footer?.replace(/#/g, 'COD-')
+  }
+};

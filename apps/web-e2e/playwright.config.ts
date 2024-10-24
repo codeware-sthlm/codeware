4a4b2931c9301fa96c6169bea93b9fae194818b2
Mathias Lundin <mathias.lundin@codeware.se>
2024-10-24 11:51:42 +0200

    chore(repo): change package manager to pnpm
    closed COD-133

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'bun nx run web:preview',
+    command: 'pnpx nx run web:preview',
     url: 'http://localhost:4300',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,

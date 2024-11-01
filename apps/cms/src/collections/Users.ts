5b3b6a8f3244fb60b05336208f2598b2379dd69c
Mathias Lundin <mathias.lundin@codeware.se>
2024-11-01 15:29:03 +0100

    feat(cms): add localization to payload config
    closed COD-149

--- apps/cms/src/collections/Users.ts
+++ apps/cms/src/collections/Users.ts
@@ -4,19 +4,29 @@ const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
   admin: { useAsTitle: 'name' },
+  labels: {
+    singular: { en: 'User', sv: 'Användare' },
+    plural: { en: 'Users', sv: 'Användare' }
+  },
   fields: [
-    { name: 'name', type: 'text', required: true },
+    {
+      name: 'name',
+      type: 'text',
+      label: { en: 'Name', sv: 'Namn' },
+      required: true
+    },
     {
       name: 'role',
       type: 'select',
+      label: { en: 'Role', sv: 'Roll' },
       options: [
-        { label: 'Admin', value: 'admin' },
-        { label: 'User', value: 'user' },
+        { label: { en: 'Admin', sv: 'Administratör' }, value: 'admin' },
+        { label: { en: 'User', sv: 'Användare' }, value: 'user' }
       ],
       required: true,
-      defaultValue: 'user',
-    },
-  ],
+      defaultValue: 'user'
+    }
+  ]
 };
 
 export default Users;

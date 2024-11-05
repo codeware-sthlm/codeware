d706ab5cd5c7e3421234574aeac45b193d4daa77
Håkan Ströberg <hakan.stroberg@codeware.se>
2024-11-05 14:40:09 +0100

    fix(nx-migrate-action): no need to check pr comments before adding a new
    closed COD-156

--- packages/nx-migrate-action/src/lib/utils/add-pull-request-comment.ts
+++ packages/nx-migrate-action/src/lib/utils/add-pull-request-comment.ts
@@ -1,28 +1,22 @@
-import { debug } from '@actions/core';
 import * as github from '@actions/github';
 import { withGitHub } from '@cx/core';
 
+import type { MigrateConfig } from './types';
+
+/**
+ * Add a comment to a pull request.
+ *
+ * @param config - The migration configuration
+ * @param pullRequest - The pull request number
+ * @param comment - The comment to add
+ */
 export const addPullRequestComment = async (
-  token: string,
+  config: MigrateConfig,
   pullRequest: number,
-  comment: string,
-  guard?: 'check-unique'
+  comment: string
 ): Promise<void> => {
+  const { token } = config;
   const octokit = github.getOctokit(token);
-
-  if (guard === 'check-unique') {
-    const { data: comments } = await withGitHub(() =>
-      octokit.rest.issues.listComments({
-        ...github.context.repo,
-        issue_number: pullRequest
-      })
-    );
-
-    if (comments.find((c) => c.body === comment)) {
-      debug(`Comment already exists in PR ${pullRequest}: '${comment}'`);
-      return;
-    }
-  }
 
   await withGitHub(() =>
     octokit.rest.issues.createComment({

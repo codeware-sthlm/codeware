f9637bb174a4729975ac85623d4d537e236263a7
Mathias Lundin <mathias.lundin@codeware.se>
2024-11-19 16:04:21 +0100

    feat(web): add navigation
    closed COD-192

--- apps/web/app/routes/about.tsx
+++ apps/web/app/routes/about.tsx
@@ -1,10 +1,10 @@
 import { Container } from '@/components/Container';
 
-export default function Index() {
+export default function About() {
   return (
     <Container className="mt-16 sm:mt-32">
       <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl">
-        Hello from web
+        About
       </h1>
     </Container>
   );

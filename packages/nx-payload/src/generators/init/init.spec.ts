37703346d5a171193461b934966058c63d7012ec
Håkan Ströberg <hakan.stroberg@codeware.se>
2024-11-19 11:55:58 +0100

    fix(nx-payload): snapshot tests should match for version patterns
    closed COD-173

--- packages/nx-payload/src/generators/init/init.spec.ts
+++ packages/nx-payload/src/generators/init/init.spec.ts
@@ -14,10 +14,26 @@ describe('init', () => {
     tree = createTreeWithEmptyWorkspace();
   });
 
-  it('should generate correct package.json', async () => {
+  it('should generate correct dependencies in package.json', async () => {
     await initGenerator(tree, {});
     const packageJson = readJson<PackageJson>(tree, 'package.json');
-    expect(packageJson).toMatchSnapshot();
+
+    // Versions number can be updated by pipelines or similar tools,
+    // so we only match for semantic patterns.
+    const dynamicVersion = /^[^|~]?\d+\.\d+\.\d+$/;
+    expect(packageJson).toMatchSnapshot({
+      dependencies: {
+        '@payloadcms/bundler-webpack': expect.stringMatching(dynamicVersion),
+        '@payloadcms/db-mongodb': expect.stringMatching(dynamicVersion),
+        '@payloadcms/db-postgres': expect.stringMatching(dynamicVersion),
+        '@payloadcms/richtext-slate': expect.stringMatching(dynamicVersion),
+        express: expect.stringMatching(dynamicVersion),
+        payload: expect.stringMatching(dynamicVersion)
+      },
+      devDependencies: {
+        '@nx/express': expect.stringMatching(/^\d+\.\d+\.\d+$/)
+      }
+    });
   });
 
   it('should keep existing dependencies', async () => {

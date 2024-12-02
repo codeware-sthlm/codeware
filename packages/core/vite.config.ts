8fb295da3b09234f826b2fb7abdb4f6be9b7f559
Mathias Lundin <mathias.lundin@codeware.se>
2024-12-02 11:43:46 +0100

    chore(core): use esm in core package

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/core2',
+  cacheDir: '../../node_modules/.vite/packages/core',
   plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
   // Uncomment this if you are using workers.
   // worker: {
@@ -17,7 +17,7 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/packages/core2',
+      reportsDirectory: '../../coverage/packages/core',
       provider: 'v8'
     },
     passWithNoTests: true

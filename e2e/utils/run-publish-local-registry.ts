78375e6747a165bded97dfd583cd64fcf62c195b
Håkan Ströberg <hakan.stroberg@codeware.se>
2024-12-08 21:24:12 +0100

    test(e2e-utils): improvements for start and stop local registry
    closed COD-110

---
+++ e2e/utils/run-publish-local-registry.ts
@@ -0,0 +1,65 @@
+import { releasePublish } from 'nx/src/command-line/release';
+import { releaseVersion } from 'nx/src/command-line/release';
+
+import { isPortInUse } from './is-port-in-use';
+
+/**
+ * This script publish to a running local registry and can be invoked standalone.
+ *
+ * ```sh
+ * nx run e2e-utils:registry-publish --projects [projects]
+ * ```
+ */
+
+const runPublishLocalRegistry = async () => {
+  const port = Number(process.env['PORT']);
+  if (!port) {
+    return '[ DONE ] PORT environment variable is not set';
+  }
+
+  if (!(await isPortInUse(port))) {
+    return `[ DONE ] Local registry on port ${port} is not started`;
+  }
+
+  const verbose = process.env['NX_VERBOSE_LOGGING'] === 'true';
+
+  const projects = process.argv
+    .slice(2)
+    .filter((a) => a !== 'true') // flag without projects
+    .join(',') // support comma separated list
+    .split(',')
+    .filter(Boolean); // remove empty strings
+
+  console.log(`Prepare to publish: ${projects.join(', ')}`);
+
+  try {
+    await releaseVersion({
+      specifier: `0.0.${Date.now()}-e2e`,
+      stageChanges: false,
+      gitCommit: false,
+      gitTag: false,
+      firstRelease: true,
+      generatorOptionsOverrides: {
+        skipLockFileUpdate: true
+      },
+      projects,
+      verbose
+    });
+
+    await releasePublish({
+      tag: 'e2e',
+      firstRelease: true,
+      projects,
+      verbose
+    });
+
+    return `[ DONE ] Published packages: ${projects.join(', ') || 'all'}`;
+  } catch (error) {
+    return `[ DONE ] Failed to publish packages: ${error}`;
+  }
+};
+
+// main
+runPublishLocalRegistry()
+  .then((msg) => console.log(`\n${msg}`))
+  .catch(console.error);

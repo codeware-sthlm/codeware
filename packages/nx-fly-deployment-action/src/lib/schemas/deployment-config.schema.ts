4175de6e36578d6aa7fbc384c2d77d43bfca9eb3
Håkan Ströberg <hakan.stroberg@codeware.se>
2024-12-10 16:23:51 +0100

    feat(nx-fly-deployment-action): add support for env and secrets
    closed COD-212

--- packages/nx-fly-deployment-action/src/lib/schemas/deployment-config.schema.ts
+++ packages/nx-fly-deployment-action/src/lib/schemas/deployment-config.schema.ts
@@ -3,12 +3,14 @@ import { z } from 'zod';
 import { ActionInputsSchema } from './action-inputs.schema';
 
 export const DeploymentConfigSchema = z.object({
+  env: z.record(z.string(), z.string()).optional(),
   fly: z.object({
     token: z.string().min(1, 'Fly API token is required'),
     org: z.string(),
     region: z.string()
   }),
   mainBranch: z.string().min(1, 'main branch is required'),
+  secrets: z.record(z.string(), z.string()).optional(),
   token: ActionInputsSchema.shape.token
 });
 
--- packages/nx-fly-deployment-action/src/lib/utils/get-deployment-config.ts
+++ packages/nx-fly-deployment-action/src/lib/utils/get-deployment-config.ts
@@ -7,6 +7,21 @@ import {
   DeploymentConfigSchema
 } from '../schemas/deployment-config.schema';
 
+const arrayToRecord = (
+  arr: Array<string>
+): Record<string, string> | undefined =>
+  arr.length
+    ? arr
+        .map((secret) => secret.split('='))
+        .reduce(
+          (acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          },
+          {} as Record<string, string>
+        )
+    : undefined;
+
 /**
  * Get deployment configuration from action inputs.
  *
@@ -21,10 +36,12 @@ export const getDeploymentConfig = async (
   inputs: ActionInputs
 ): Promise<DeploymentConfig> => {
   const {
+    env: envInput,
     flyApiToken,
     flyOrg,
     flyRegion,
     mainBranch: mainBranchInput,
+    secrets: secretsInput,
     token
   } = inputs;
 
@@ -32,13 +49,21 @@ export const getDeploymentConfig = async (
   const mainBranch =
     mainBranchInput || (await getRepositoryDefaultBranch(token));
 
+  // Parse environment variables from input
+  const env = arrayToRecord(envInput);
+
+  // Parse secrets from input
+  const secrets = arrayToRecord(secretsInput);
+
   const config: DeploymentConfig = {
+    env,
     fly: {
       token: flyApiToken || process.env['FLY_API_TOKEN'] || '',
       org: flyOrg,
       region: flyRegion
     },
     mainBranch,
+    secrets,
     token
   };
   core.info(JSON.stringify(config, null, 2));

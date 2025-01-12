c7c940b52d54c0573ee5c8d718dae040fbe50e86
Håkan Ströberg <hakan.stroberg@codeware.se>
2025-01-12 21:18:22 +0100

    feat(core): withEnvVars support both object and props level
    closed COD-230

--- packages/core/src/lib/zod/with-env-vars.preprocess.spec.ts
+++ packages/core/src/lib/zod/with-env-vars.preprocess.spec.ts
@@ -3,69 +3,119 @@ import { z } from 'zod';
 import { withEnvVars } from './with-env-vars.preprocess';
 
 describe('withEnvVars', () => {
-  let originalTestEnvVar: string | undefined;
-
+  let originalTestApiUrl: string | undefined;
+  let originalTestKey: string | undefined;
   beforeAll(() => {
-    originalTestEnvVar = process.env['TEST_API_URL'];
+    originalTestApiUrl = process.env['TEST_API_URL'];
+    originalTestKey = process.env['TEST_KEY'];
   });
 
   beforeEach(() => {
     process.env['TEST_API_URL'] = 'https://api.example.com';
+    process.env['TEST_KEY'] = 'k3y';
   });
 
   afterEach(() => {
-    if (originalTestEnvVar) {
-      process.env['TEST_API_URL'] = originalTestEnvVar;
+    if (originalTestApiUrl) {
+      process.env['TEST_API_URL'] = originalTestApiUrl;
     } else {
       delete process.env['TEST_API_URL'];
     }
+    if (originalTestKey) {
+      process.env['TEST_KEY'] = originalTestKey;
+    } else {
+      delete process.env['TEST_KEY'];
+    }
   });
 
-  it('should replace correctly without options', () => {
+  it('should apply to object without options', () => {
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string().url(),
+        secret: z.string()
+      })
+    );
+
+    expect(
+      schema.parse({ apiUrl: '{TEST_API_URL}', secret: 'my-{TEST_KEY}' })
+    ).toEqual({
+      apiUrl: 'https://api.example.com',
+      secret: 'my-k3y'
+    });
+  });
+
+  it('should apply to a property of an object with options', () => {
     const schema = z.object({
-      apiUrl: withEnvVars(z.string().url())
+      apiUrl: withEnvVars(z.string().url(), { prefix: 'TEST_' }),
+      secret: z.string()
     });
 
-    expect(schema.parse({ apiUrl: '{TEST_API_URL}' })).toEqual({
-      apiUrl: 'https://api.example.com'
+    expect(
+      schema.parse({ apiUrl: '{API_URL}', secret: 'my-{TEST_KEY}' })
+    ).toEqual({
+      apiUrl: 'https://api.example.com',
+      secret: 'my-{TEST_KEY}'
     });
   });
 
   it('should support $ in the environment variables', () => {
-    const schema = z.object({
-      apiUrl: withEnvVars(z.string().url())
-    });
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string().url(),
+        secret: z.string()
+      })
+    );
 
-    expect(schema.parse({ apiUrl: '${TEST_API_URL}' })).toEqual({
-      apiUrl: 'https://api.example.com'
+    expect(
+      schema.parse({ apiUrl: '${TEST_API_URL}', secret: 'my-${TEST_KEY}' })
+    ).toEqual({
+      apiUrl: 'https://api.example.com',
+      secret: 'my-k3y'
     });
   });
 
-  it('should throw an error if the environment variable is not found', () => {
-    const schema = z.object({
-      apiUrl: withEnvVars(z.string().url())
-    });
+  it('should throw an error if environment variable is not found', () => {
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string().url(),
+        secret: z.string()
+      }),
+      {
+        throwOnMissing: true
+      }
+    );
 
-    expect(() => schema.parse({ apiUrl: '{API_URL}' })).toThrow();
+    expect(() =>
+      schema.parse({ apiUrl: '{API_URL}', secret: 'my-{TEST_KEY}' })
+    ).toThrow();
   });
 
   it('should use a default value if the environment variable is not found', () => {
-    const schema = z.object({
-      apiUrl: withEnvVars(z.string().url(), {
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string().url(),
+        secret: z.string()
+      }),
+      {
         defaultValue: 'http://localhost:3000'
-      })
-    });
+      }
+    );
 
-    expect(schema.parse({ apiUrl: '{API_URL}' })).toEqual({
-      apiUrl: 'http://localhost:3000'
+    expect(
+      schema.parse({ apiUrl: '{API_URL}', secret: 'my-{TEST_KEY}' })
+    ).toEqual({
+      apiUrl: 'http://localhost:3000',
+      secret: 'my-k3y'
     });
   });
 
   it('should treat undefined as normal value', () => {
     process.env['TEST_API_URL'] = undefined;
-    const schema = z.object({
-      apiUrl: withEnvVars(z.string())
-    });
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string()
+      })
+    );
 
     expect(schema.parse({ apiUrl: '${TEST_API_URL}' })).toEqual({
       apiUrl: 'undefined'
@@ -74,9 +124,14 @@ describe('withEnvVars', () => {
 
   it('should treat undefined as an empty string', () => {
     process.env['TEST_API_URL'] = undefined;
-    const schema = z.object({
-      apiUrl: withEnvVars(z.string(), { undefinedIsEmpty: true })
-    });
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string()
+      }),
+      {
+        undefinedIsEmpty: true
+      }
+    );
 
     expect(schema.parse({ apiUrl: '${TEST_API_URL}' })).toEqual({
       apiUrl: ''
@@ -84,9 +139,12 @@ describe('withEnvVars', () => {
   });
 
   it('should use a prefix to match environment variable', () => {
-    const schema = z.object({
-      apiUrl: withEnvVars(z.string().url(), { prefix: 'TEST_' })
-    });
+    const schema = withEnvVars(
+      z.object({
+        apiUrl: z.string().url()
+      }),
+      { prefix: 'TEST_' }
+    );
 
     expect(schema.parse({ apiUrl: '{API_URL}' })).toEqual({
       apiUrl: 'https://api.example.com'
--- packages/nx-fly-deployment-action/src/lib/schemas/github-config.schema.ts
+++ packages/nx-fly-deployment-action/src/lib/schemas/github-config.schema.ts
@@ -1,23 +1,21 @@
 import { withEnvVars } from '@codeware/core/zod';
 import { z } from 'zod';
 
-export const GitHubConfigSchema = z.object({
-  deploy: z.boolean(),
-  flyConfig: z.string().regex(/fly\.toml$/, 'fly.toml file is required'),
-  flyPostgresPreview: withEnvVars(
-    z
+export const GitHubConfigSchema = withEnvVars(
+  z.object({
+    deploy: z.boolean(),
+    flyConfig: z.string().regex(/fly\.toml$/, 'fly.toml file is required'),
+    flyPostgresPreview: z
       .string()
       .regex(/^[a-zA-Z0-9_-]*$/, 'flyPostgresPreview pattern is invalid')
       .optional(),
-    { throwOnMissing: true, undefinedIsEmpty: true }
-  ),
-  flyPostgresProduction: withEnvVars(
-    z
+
+    flyPostgresProduction: z
       .string()
       .regex(/^[a-zA-Z0-9_-]*$/, 'flyPostgresProduction pattern is invalid')
-      .optional(),
-    { throwOnMissing: true, undefinedIsEmpty: true }
-  )
-});
+      .optional()
+  }),
+  { throwOnMissing: true, undefinedIsEmpty: true }
+);
 
 export type GitHubConfig = z.infer<typeof GitHubConfigSchema>;

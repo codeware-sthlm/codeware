2e0f3257fb0c16528139b090c8c2648f104026a7
Håkan Ströberg <hakan.stroberg@codeware.se>
2025-01-28 15:13:42 +0100

    fix(cms): temporary remove flaky lexical code block
    closed COD-260

--- libs/shared/ui/react-components/src/index.ts
+++ libs/shared/ui/react-components/src/index.ts
@@ -1,4 +1,8 @@
 export { CdwrCloud } from './lib/cdwr-cloud';
 export { CdwrAdminLogo } from './lib/payload-components/cdwr-admin-logo';
-export { Code, type Language } from './lib/payload-components/code';
+export {
+  Code,
+  type Language,
+  languageMap
+} from './lib/payload-components/code';
 export { LexicalRenderer } from './lib/payload-components/lexical-renderer/lexical-renderer';

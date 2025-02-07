0b801c50e862374900f7f00c344ff9a999311030
Håkan Ströberg <hakan.stroberg@codeware.se>
2025-02-07 15:16:28 +0100

    refactor(core): move secrets and some zod utilities to internal projects
    BREAKING CHANGE COD-263

--- packages/core/src/secrets.ts
+++
@@ -1,6 +0,0 @@
-export {
-  type Environment,
-  EnvironmentSchema
-} from './lib/secrets/infisical.schemas';
-export { readInfisicalConfig } from './lib/secrets/read-infisical-config';
-export { withInfisical } from './lib/secrets/with-infisical';
--- packages/core/src/zod.ts
+++ packages/core/src/zod.ts
@@ -1,5 +1,3 @@
-export { createLowercaseSchema } from './lib/zod/create-lowercase-schema';
 export { JsonSchema } from './lib/zod/json.schema';
 export { withCamelCase } from './lib/zod/with-camel-case.preprocess';
-export { withEnvVars } from './lib/zod/with-env-vars.preprocess';
 export { withReplaceAll } from './lib/zod/with-replace-all.preprocess';

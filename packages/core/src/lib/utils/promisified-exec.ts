bbb3e3b9f391475bae01c68fda14af25a33725e7
Håkan Ströberg <hakan.stroberg@codeware.se>
2025-02-14 10:09:23 +0100

    fix(core): improve windows compatibility
    closed COD-203

--- packages/core/src/lib/utils/promisified-exec.ts
+++ packages/core/src/lib/utils/promisified-exec.ts
@@ -1,7 +1,12 @@
-import { exec as cpExec } from 'child_process';
+import { exec as cpExec, execFile as cpExecFile } from 'child_process';
 import { promisify } from 'util';
 
 /**
  * Promisified version of the `exec` function from the `child_process` module.
  */
 export const exec = promisify(cpExec);
+
+/**
+ * Promisified version of the `execFile` function from the `child_process` module.
+ */
+export const execFile = promisify(cpExecFile);

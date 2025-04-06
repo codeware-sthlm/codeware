aee2520bd4ec9725a12bb7c26c191f5aaa2fcd0e
Håkan Ströberg <hakan.stroberg@codeware.se>
2025-04-06 23:33:00 +0200

    fix(repo): setup tailwind and shadcn properly for agnostic support
    Also added theme support to cms frontend app
    
    closed COD-311

--- apps/cms/src/app/(frontend)/layout.tsx
+++ apps/cms/src/app/(frontend)/layout.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { cn } from '@codeware/shared/util/ui';
 
 import './globals.css';
+import { ThemeProvider } from './components/theme-provider';
+import { ModeToggle } from './components/ThemeToggle.client';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,29 +20,44 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={cn('flex min-h-screen flex-col', inter.className)}>
-        <main className="mx-auto flex w-full max-w-2xl flex-1 flex-col justify-center p-6 sm:p-8">
-          {children}
-        </main>
-        <footer className="mx-auto flex flex-wrap justify-center gap-4 p-6 text-sm opacity-70 hover:opacity-100 hover:transition-opacity sm:flex-nowrap sm:gap-8 sm:p-8">
-          <a
-            className="relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-right after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100"
-            href="https://github.com/codeware-sthlm/codeware/tree/main/packages/nx-payload"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            @cdwr/payload
-          </a>
-          <a
-            className="relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-right after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </footer>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'bg-cdwr-light-gray/50 dark:text-cdwr-light-gray text-cdwr-space-cadet dark:bg-cdwr-darker-black flex min-h-screen flex-col',
+          inter.className
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="flex justify-end p-6 sm:p-8">
+            <ModeToggle />
+          </header>
+          <main className="mx-auto flex w-full max-w-2xl flex-1 flex-col justify-center p-6 sm:p-8">
+            {children}
+          </main>
+          <footer className="mx-auto flex flex-wrap justify-center gap-4 p-6 text-sm opacity-70 hover:opacity-100 hover:transition-opacity sm:flex-nowrap sm:gap-8 sm:p-8">
+            <a
+              className="relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-right after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100"
+              href="https://github.com/codeware-sthlm/codeware/tree/main/packages/nx-payload"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              @cdwr/payload
+            </a>
+            <a
+              className="relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-right after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100"
+              href="https://payloadcms.com/docs"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Documentation
+            </a>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );

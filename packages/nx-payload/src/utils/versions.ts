191cfcfefc4c64bbf2e818fdd510d88c0981128b
Håkan Ströberg <hakan.stroberg@codeware.se>
2025-09-27 00:41:54 +0200

    fix(nx-payload): stay on older payload version due to pnpm issue

--- packages/nx-payload/src/utils/versions.ts
+++ packages/nx-payload/src/utils/versions.ts
@@ -1,5 +1,10 @@
-/** Payload version which is in sync with all plugins */
-export const payloadVersion = '^3.20.0';
+/**
+ * Payload version which is in sync with all plugins.
+ *
+ * Due to an issue for pnpm workspaces, we currently stay at 3.42.
+ * https://github.com/payloadcms/payload/pull/12622
+ */
+export const payloadVersion = '~3.42.0';
 
 export const graphqlVersion = '^16.10.0';
 

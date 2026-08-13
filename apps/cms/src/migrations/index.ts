41703ae7f055b7b7664e1c2b6fed28113fa8252d
Håkan Ströberg <hakan.stroberg@codeware.se>
2026-08-14 00:44:20 +0200

    chore(cms): migrate the workspace domains schema

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -42,6 +42,7 @@ import * as migration_20260809_085500_cod_376_platform_labels from './20260809_0
 import * as migration_20260809_204500_cod_376_stock_media_prefix from './20260809_204500_cod_376_stock_media_prefix';
 import * as migration_20260809_231943_cod_432 from './20260809_231943_cod_432';
 import * as migration_20260812_212841_cod_435 from './20260812_212841_cod_435';
+import * as migration_20260813_224231_cod_357 from './20260813_224231_cod_357';
 
 export const migrations = [
   {
@@ -263,5 +264,10 @@ export const migrations = [
     up: migration_20260812_212841_cod_435.up,
     down: migration_20260812_212841_cod_435.down,
     name: '20260812_212841_cod_435'
+  },
+  {
+    up: migration_20260813_224231_cod_357.up,
+    down: migration_20260813_224231_cod_357.down,
+    name: '20260813_224231_cod_357'
   }
 ];
